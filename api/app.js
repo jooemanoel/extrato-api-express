d24c79a18f576514f99c0337bfdedc2396709f77
Joao Emanoel Rodrigues Mota <jooemanoel@gmail.com>
2025-11-20 16:27:52 -0300

    Remoção dos endpoints ofx

--- api/app.js
+++ api/app.js
@@ -7,7 +7,6 @@ import rootRoutes from "./modules/root/root.routes.js";
 import compraRoutes from "./modules/compras/compra.routes.js";
 import faturaRoutes from "./modules/fatura/fatura.routes.js";
 import usuarioRoutes from "./modules/usuario/usuario.routes.js";
-import ofxRoutes from "./modules/ofx/ofx.routes.js";
 
 export function createApp() {
   const app = express();
@@ -17,7 +16,6 @@ export function createApp() {
   app.use('/compras', compraRoutes);
   app.use('/faturas', faturaRoutes);
   app.use('/usuario', usuarioRoutes);
-  // app.use('/ofx', ofxRoutes);
   return app;
 }
 
