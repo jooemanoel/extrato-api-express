d126ddb448d3388cd20549599ebf6b6d7e514be7
Joao Emanoel Rodrigues Mota <jooemanoel@gmail.com>
2025-11-23 22:19:49 -0300

    Compras por fatura

--- api/modules/compras/compra.dao.js
+++ api/modules/compras/compra.dao.js
@@ -4,7 +4,7 @@ import sql from "../../config/db.js";
 
 export const listar = async (codigo_usuario) => {
   return await sql`
-    SELECT fitid, trntype, descricao_compra, data_compra, valor_compra, codigo_categoria_compra
+    SELECT fitid, trntype, descricao_compra, data_compra, valor_compra, codigo_categoria_compra, codigo_fatura
     FROM public.compra
     WHERE codigo_usuario = ${codigo_usuario}
     ORDER BY data_compra DESC, descricao_compra DESC;
@@ -13,10 +13,20 @@ export const listar = async (codigo_usuario) => {
 
 export const listarPorData = async (codigo_usuario, data_abertura_fatura, data_fechamento_fatura) => {
   return await sql`
-    SELECT fitid, trntype, descricao_compra, data_compra, valor_compra, codigo_categoria_compra
+    SELECT fitid, trntype, descricao_compra, data_compra, valor_compra, codigo_categoria_compra, codigo_fatura
     FROM public.compra
     WHERE codigo_usuario = ${codigo_usuario}
     AND data_compra BETWEEN ${data_abertura_fatura} AND ${data_fechamento_fatura}
+    ORDER BY data_compra DESC, descricao_compra DESC;
+  `;
+};
+
+export const listarPorFatura = async (codigo_usuario, codigo_fatura) => {
+  return await sql`
+    SELECT fitid, trntype, descricao_compra, data_compra, valor_compra, codigo_categoria_compra, codigo_fatura
+    FROM public.compra
+    WHERE codigo_usuario = ${codigo_usuario}
+    AND codigo_fatura = ${codigo_fatura}
     ORDER BY data_compra DESC, descricao_compra DESC;
   `;
 };
@@ -32,7 +42,7 @@ export const inserir = async ({
 }) => {
   const [compra] = await sql`
     INSERT INTO public.compra
-      (fitid, trntype, descricao_compra, data_compra, valor_compra, codigo_categoria_compra, codigo_usuario)
+      (fitid, trntype, descricao_compra, data_compra, valor_compra, codigo_categoria_compra, codigo_fatura, codigo_usuario)
     VALUES (
       ${fitid},
       ${trntype},
@@ -40,11 +50,13 @@ export const inserir = async ({
       ${data_compra},
       ${valor_compra},
       ${codigo_categoria_compra},
+      ${codigo_fatura},
       ${codigo_usuario}
     )
     ON CONFLICT (fitid) DO NOTHING
     RETURNING fitid;
   `;
+  console.log('compra.dao - inserir', compra);
   return compra;
 };
 
@@ -53,16 +65,19 @@ export const editar = async (fitid, {
   data_compra,
   valor_compra,
   codigo_categoria_compra,
+  codigo_fatura
 }) => {
   const [compra] = await sql`
     UPDATE public.compra
     SET descricao_compra = ${descricao_compra},
         data_compra = ${data_compra},
         valor_compra = ${valor_compra},
-        codigo_categoria_compra = ${codigo_categoria_compra}
+        codigo_categoria_compra = ${codigo_categoria_compra},
+        codigo_fatura = ${codigo_fatura}
     WHERE fitid = ${fitid}
-    RETURNING fitid, trntype, descricao_compra, data_compra, valor_compra, codigo_categoria_compra;
+    RETURNING fitid, trntype, descricao_compra, data_compra, valor_compra, codigo_categoria_compra, codigo_fatura;
   `;
+  console.log('compra.dao - editar', compra);
   return compra;
 };
 
@@ -72,5 +87,6 @@ export const apagar = async (fitid) => {
     WHERE fitid = ${fitid}
     RETURNING fitid, descricao_compra;
   `;
+  console.log('compra.dao - apagar', compra);
   return compra;
 };
